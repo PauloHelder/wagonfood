546ce5b70a4be858e5c5541cf056608226235e20
pphelder <pphelder@gmail.com>
2024-07-22 00:21:48 +0100

    ultima versão

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 Route::get('/tenants/{uuid}','Api\TenantApiController@show');
 Route::get('/tenants','Api\TenantApiController@index');
-
 Route::get('/categories','Api\CategoryApiController@categoriesByTenant');
-
 Route::get('/tables','Api\TableApiController@tabesByTenant');
